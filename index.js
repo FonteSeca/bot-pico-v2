145c8a5fd2e642f805047e42039b32a0e72b49e6
yFonteSeca <lf.caetanofonseca@outlook.com>
2017-09-29 08:24:54 -0300

    nha

--- index.js
+++ index.js
@@ -172,19 +172,12 @@ Bot.on('message', msg => {
   }
 	
 	function purge(msg, suffix) {
-	// This command removes all messages from all users in the channel, up to 100.
-    
-    // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 10);
-    
-    // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return msg.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({count: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => msg.reply(`Couldn't delete messages because of: ${error}`));	
+	        if (message.member.hasPermission("MANAGE_MESSAGES")) {
+            msg.channel.fetchMessages()
+               .then(function(list){
+                    msg.channel.bulkDelete(list);
+                }, function(err){msg.channel.send("ERROR: ERROR CLEARING CHANNEL.")})                        
+        }
 	}
 
   function kiss(msg,suffix){
