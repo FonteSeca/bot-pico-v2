7ff97c4434126b571e2525eda299e93776126e06
yFonteSeca <lf.caetanofonseca@outlook.com>
2017-10-06 13:08:21 -0300

    aa

--- music.js
+++ music.js
@@ -158,6 +158,34 @@ module.exports = function (client, options) {
 		if (queue.length >= MAX_QUEUE_SIZE) {
 			return msg.channel.send(wrap('16766720',':x: :white_small_square:  Playlist cheia! Espere esvaziar'));
 		}
+		
+		const embede = new Discord.RichEmbed()
+  .setTitle("This is your title, it can hold 256 characters")
+  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor(0x00AE86)
+  .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+  .setImage("http://i.imgur.com/yVpymuV.png")
+  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  /*
+   * Takes a Date object, defaults to current date.
+   */
+  .setTimestamp()
+  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+  .addField("This is a field title, it can hold 256 characters",
+    "This is a field value, it can hold 2048 characters.")
+  /*
+   * Inline fields may not display as inline if the thumbnail and/or image is too big.
+   */
+  .addField("Inline Field", "They can also be inline.", true)
+  /*
+   * Blank field, useful to create some space.
+   */
+  .addBlankField(true)
+  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
 
 		// Get the video information.
 		msg.channel.send(wrap('16766720',':musical_note: :white_small_square:  **Procurando...**')).then(response => {
@@ -172,6 +200,7 @@ module.exports = function (client, options) {
 				if (err || info.format_id === undefined || info.format_id.startsWith('0')) {
 					console.log(info);
 					return response.edit(wrap('16766720',':x: :white_small_square:  **Vídeo inválido!**'));
+				
 				}
 
 				info.requester = msg.author.id;
@@ -179,6 +208,7 @@ module.exports = function (client, options) {
 				// Queue the video.
 				response.edit(wrap('16766720',':musical_note: :white_small_square:  **Adicionado na playlist:** *' + info.title + '*')).then(() => {
 					queue.push(info);
+					msg.channel.send({embede});
 					// Play if only one element in the queue.
 					if (queue.length === 1) executeQueue(msg, queue);
 				}).catch(console.log);
