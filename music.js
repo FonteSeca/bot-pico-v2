9b3d28deeb9225d067256f0a2a246f30f2dec1c3
yFonteSeca <lf.caetanofonseca@outlook.com>
2017-10-07 21:37:35 -0300

    asdasd

--- music.js
+++ music.js
@@ -188,7 +188,7 @@ module.exports = function (client, options) {
 					    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
 					    "text": "Pico | YouTube"
 					  },
-					  "image": {
+					  "thumbnail": {
 					    "url": info.thumbnail
 					  },
 
