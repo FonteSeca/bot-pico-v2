2a1b259c436cf6020a8b11c39da6ec8f4b04964b
yFonteSeca <lf.caetanofonseca@outlook.com>
2017-10-19 23:06:30 -0200

    asd

--- music.js
+++ music.js
@@ -207,7 +207,7 @@ module.exports = function (client, options) {
 					    },
 					    {
 					      "name": "Descrição",
-					      "value": info.description,
+					      "value": "nha",
 					      "inline": true
 					    }
 					  ]
